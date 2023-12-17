aeb8cc1f7c2eeb66c4a3dbe5d8487b8f5430de95
Jaydee <jaydee.tarpeh@gmail.com>
2023-12-17 19:19:58 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+let multiply = num1 * num2
+
+// test 2
+
+let random = Math.floor(Math.random() * 100) + 1;
+
+// test 3
+
+let num3 = 100
+let num4 = 6
+
+let mod = num3 % num4
+
+// test 4
+
+let max = Math.max(3, 20, 13, 8, 1)
+
+console.log(max)
